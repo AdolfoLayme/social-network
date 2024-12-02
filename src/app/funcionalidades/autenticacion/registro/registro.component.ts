6b64f18f0167f5661b3e8be635561f61a2db62c0
Adolfo Layme <laymebonifacioa@gmail.com>
2024-12-01 21:08:53 -0500

    Mejorando la pagina de registro, implementando el modal de carga con los mensajes

--- src/app/funcionalidades/autenticacion/registro/registro.component.ts
+++ src/app/funcionalidades/autenticacion/registro/registro.component.ts
@@ -17,39 +17,51 @@ export class RegistroComponent {
   email: string = '';
   password: string = '';
 
-  constructor(
-    private authService: UsuarioService,
-    private firestore: Firestore,
-    private router: Router
-  ) {}
+  mensajeModal: string = '';
+  tipoMensaje: 'error' | 'exito' | '' = ''; 
+  cargando: boolean = false; 
+  mostrarModal: boolean = false; 
+
+  constructor(private authService: UsuarioService, private firestore: Firestore, private router: Router) {}
 
   async onRegister() {
+    this.cargando = true; 
     try {
       // Registrar usuario con Firebase Authentication
       const res = await this.authService.register(this.email, this.password);
-
       // Guardar datos adicionales del usuario en Firestore
       const uid = res.user?.uid;
-      if (uid) {
-        const usuarioRef = doc(this.firestore, `usuarios/${uid}`);
-        await setDoc(usuarioRef, {
-          nombre: this.nombre,
-          email: this.email,
-          createdAt: new Date(),
-        });
-      }
+      const usuarioRef = doc(this.firestore, `usuarios/${uid}`);
+      await setDoc(usuarioRef, {
+        nombre: this.nombre,
+        email: this.email,
+        createdAt: new Date(),
+      });
 
+      this.mensajeModal = 'Usuario registrado exitosamente.';
+      this.tipoMensaje = 'exito';
+      this.mostrarModal = true;
       // Limpiar formulario
       this.nombre = '';
       this.email = '';
       this.password = '';
-
-      // Redirigir 
+      this.cargando = false;
+      // Redirigir después de mostrar el mensaje de éxito
       setTimeout(() => {
+        this.mostrarModal = false;
         this.router.navigate(['/home']);
       }, 3000);
-    } catch (err) {
-      console.error('Error al registrar usuario:', err);
+    } catch (err: any) {
+      // Configurar el modal de error
+      this.mensajeModal = `Error al registrar usuario: ${err.message}`;
+      this.tipoMensaje = 'error';
+      this.mostrarModal = true;
+      this.cargando = false;
+
+      // Ocultar el mensaje de error 
+      setTimeout(() => {
+        this.mostrarModal = false;
+      }, 3000);
     }
   }
 }
