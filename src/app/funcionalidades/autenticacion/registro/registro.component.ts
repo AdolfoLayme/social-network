af6b6df7f58ba4ece2f3ba2e67296ee17387341e
Adolfo Layme <laymebonifacioa@gmail.com>
2024-12-08 04:48:59 -0500

    Mejorando el componente de Autenticación

--- src/app/funcionalidades/autenticacion/registro/registro.component.ts
+++ src/app/funcionalidades/autenticacion/registro/registro.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Router} from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { UsuarioService } from '../../../core/servicios/usuario.service'; 
+import { UsuarioService } from '../../../core/servicios/usuario.service';
 import { Firestore, doc, setDoc } from '@angular/fire/firestore';
 
 @Component({
@@ -10,27 +10,36 @@ import { Firestore, doc, setDoc } from '@angular/fire/firestore';
   standalone: true,
   imports: [FormsModule, RouterModule, CommonModule],
   templateUrl: './registro.component.html',
-  styleUrl: './registro.component.css'
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent {
   nombre: string = '';
   email: string = '';
   password: string = '';
-
+  confirmPassword: string = '';
+  mensajeError: string = '';
   mensajeModal: string = '';
-  tipoMensaje: 'error' | 'exito' | '' = ''; 
-  cargando: boolean = false; 
-  mostrarModal: boolean = false; 
+  tipoMensaje: 'error' | 'exito' | '' = '';
+  cargando: boolean = false;
+  mostrarModal: boolean = false;
 
-  constructor(private authService: UsuarioService, private firestore: Firestore, private router: Router) {}
+  constructor(
+    private authService: UsuarioService,
+    private firestore: Firestore,
+    private router: Router
+  ) {}
 
   async onRegister() {
-    this.cargando = true; 
+    if (this.password !== this.confirmPassword) {
+      this.mensajeError = 'Las contraseñas no coinciden.';
+      return;
+    }
+
+    this.cargando = true;
     try {
-      // Registrar usuario con Firebase Authentication
       const res = await this.authService.register(this.email, this.password);
-      // Guardar datos adicionales del usuario en Firestore
       const uid = res.user?.uid;
+
       const usuarioRef = doc(this.firestore, `usuarios/${uid}`);
       await setDoc(usuarioRef, {
         nombre: this.nombre,
@@ -41,24 +50,24 @@ export class RegistroComponent {
       this.mensajeModal = 'Usuario registrado exitosamente.';
       this.tipoMensaje = 'exito';
       this.mostrarModal = true;
-      // Limpiar formulario
+
       this.nombre = '';
       this.email = '';
       this.password = '';
+      this.confirmPassword = '';
       this.cargando = false;
-      // Redirigir después de mostrar el mensaje de éxito
+
       setTimeout(() => {
         this.mostrarModal = false;
         this.router.navigate(['/home']);
       }, 3000);
     } catch (err: any) {
-      // Configurar el modal de error
+ 
       this.mensajeModal = `Error al registrar usuario: ${err.message}`;
       this.tipoMensaje = 'error';
       this.mostrarModal = true;
       this.cargando = false;
 
-      // Ocultar el mensaje de error 
       setTimeout(() => {
         this.mostrarModal = false;
       }, 3000);
