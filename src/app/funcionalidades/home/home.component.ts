08eb9d7d9cf4cdad16ca1a7ba3295355ea770be9
Adolfo Layme <laymebonifacioa@gmail.com>
2024-12-08 04:06:44 -0500

    Mejorando el login

--- src/app/funcionalidades/home/home.component.ts
+++ src/app/funcionalidades/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms'; 
-import { RouterModule, Router} from '@angular/router'; 
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Router } from '@angular/router';
 import { UsuarioService } from '../../core/servicios/usuario.service';
 import { CommonModule } from '@angular/common';
 
@@ -9,29 +9,42 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [FormsModule, RouterModule, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
   email: string = '';
   password: string = '';
   mensajeError: string = '';
+  correoInvalido: boolean = false;
   cargando: boolean = false;
 
   constructor(private usuarioService: UsuarioService, private router: Router) {}
 
+  validarCorreoInstitucional(): void {
+    const correoRegex = /^[a-zA-Z0-9._%+-]+@unajma\.edu\.pe$/;
+    this.correoInvalido = !correoRegex.test(this.email);
+  }
+  
+
   async onLogin() {
-    this.cargando = true; 
-    this.mensajeError = ''; 
+    this.mensajeError = '';
+
+    if (this.correoInvalido) {
+      this.mensajeError = 'Por favor, utiliza un correo institucional válido.';
+      return;
+    }
+
+    this.cargando = true;
 
     try {
       // Iniciar sesión
       await this.usuarioService.login(this.email, this.password);
-      this.cargando = false; 
-      this.router.navigate(['/configurar-perfil']); 
+      this.router.navigate(['/configurar-perfil']);
     } catch (error: any) {
-      this.cargando = false;
       this.mensajeError = 'Error al iniciar sesión. Verifica tus credenciales.';
       console.error('Error en inicio de sesión:', error);
+    } finally {
+      this.cargando = false;
     }
   }
 }
